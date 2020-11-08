fc01c5d151631bca769298e64f85b485a18ff34f
noelia2016 <noeliacarrizo22@gmail.com>
2020-11-08 00:22:57 -0300

    actualizacion de componentes y vista de canchas

--- src/app/cancha-list/cancha-list.component.ts
+++ src/app/cancha-list/cancha-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cancha } from './cancha';
 
 @Component({
   selector: 'app-cancha-list',
@@ -7,13 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CanchaListComponent implements OnInit {
 
-  cancha = {
-    "nro": 1,
-	"tipo": "sintetico",
-	"cant_jugadores": 5,
-	"precio": 50,
-	"turnos_disponibles": 5
-  }
+  canchas: Cancha [] = [
+
+    {
+      "nro": 1,
+    	"tipo": "sintetico",
+    	"cant_jugadores": 5,
+    	"precio": 50,
+    	"turnos_disponibles": 5,
+    },
+
+    {
+      "nro": 2,
+      "tipo": "cesped",
+      "cant_jugadores": 11,
+      "precio": 350,
+      "turnos_disponibles": 1,
+    },
+
+    {
+      "nro": 3,
+      "tipo": "sintetico",
+      "cant_jugadores": 7,
+      "precio": 150,
+      "turnos_disponibles": 2,
+    }, 
+
+    {
+      "nro": 4,
+      "tipo": "cesped",
+      "cant_jugadores": 8,
+      "precio": 250,
+      "turnos_disponibles": 5,
+    },
+
+    {
+      "nro": 5,
+      "tipo": "sintetico",
+      "cant_jugadores": 5,
+      "precio": 180,
+      "turnos_disponibles": 0,
+    },
+
+  ]
 
   constructor() { }
 
---
+++ src/app/cancha-list/cancha.ts
@@ -0,0 +1,7 @@
+export interface Cancha {
+	nro: number;
+	tipo: string,
+	cant_jugadores: number;
+	precio: number;
+	turnos_disponibles: number;
+} 
